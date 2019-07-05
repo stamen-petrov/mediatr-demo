9dca17c420e1ba49c52804c4a2a7a92395eccdee
Stamen Petrov <stamen.petrov@gmail.com>
2019-07-05 18:22:44 +0300

    aaa

--- MediatrDemo/MediatrDemoProcessors/Program.cs
+++ MediatrDemo/MediatrDemoProcessors/Program.cs
@@ -6,7 +6,7 @@ namespace MediatrDemoProcessors
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World from testbranch1!");
         }
     }
 }
