52fbed42729de76c4515ce2aaddfa594af8ba334
Stamen Petrov <stamen.petrov@gmail.com>
2019-07-05 19:16:26 +0300

    first pull request!

--- MediatrDemo/MediatrDemoProcessors/Program.cs
+++ MediatrDemo/MediatrDemoProcessors/Program.cs
@@ -7,6 +7,8 @@ namespace MediatrDemoProcessors
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World from testbranch1!");
+
+            Console.WriteLine("First pull request!");
         }
     }
 }
